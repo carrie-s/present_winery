e73c507d920c5be79fcfd2016686af56431275f3
carrie-s <59153013+carrie-s@users.noreply.github.com>
2020-01-03 17:02:35 +0800

    2020-01-03 index
    index(news.products)

--- index.php
+++ index.php
@@ -21,26 +21,38 @@
         <img src="images/01.png" alt="winery">
     </header>
     <section>
-        <div class="news">
-            <div class="title">
-            <img class="titleimg" src="images/title.png" alt="news">
-            <span>最 新 消 息</span>
+        <div class="ns-block">
+            <div class="ns-title">
+            <img class="ns-titleimg" src="images/news2.png" alt="news">
+            <!-- <span>最 新 消 息</span> -->
             </div>
-            <div class="newscontent">
-                <div class="newsimg">
-                    <img src="images/02.jpg" width="500">
+            <div class="ns-content">
+                <div class="news-item">
+                    <img src="images/02.jpg" width="100%">
                 </div>
                 <div class="newstext">
+                    <p class="newstitle">2020終極大福箱</p>
                     <p>酒瓶子一年一度最大盛事，大福箱開始預購！越是不景氣，越要反向操作，去年大福箱獎品總額15萬，今年直接拉到20萬，老樣子，大家開心最重要！</p>
-                    
                 </div>
-                <div class="newsbg"></div>
+                <div class="news-item">
+                    <div>
+                        <dl>
+                            <dt>2020尾牙春酒精選</dt>
+                            <dd>單瓶滿12瓶免運，或與其他活動或組合合購，滿12瓶也可以享箱價優惠唷！</dd>
+                            <dt>絕版熟成聖夜喬治一級園</dt>
+                            <dd>絕版酒不是重點，而是現在07年熟得完美！鮮明的紅花香，紅莓、黑莓香氣交錯，口感飽滿完整，12年熟成的細緻口感，加上回味的礦石感與細緻香料提示，好一支巔峰熟成的勃根地~</dd>
+                            <dt>夏日白酒節組</dt>
+                            <dd>每年夏天酒瓶子都會推出白酒組，每年都受到各位酒友的好評，再次感謝大家，今年的白酒節再升級！</dd>
+                        </dl>
+                    </div>
+                </div>
+
+                <div class="learnmore clear-both"><div class="newsbtn">了解更多</div></div>
             </div>
         </div>
         <div class="product clear-both">
-            <div class="title">
-                <img class="titleimg" src="images/title.png" alt="">
-                <span>熱 銷 商 品</span>
+        <div class="ns-title">
+            <img class="ns-titleimg" src="images/hotproducts.png" alt="hotproducts">
             </div>
 
 
